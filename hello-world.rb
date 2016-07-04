b9ae7103921ad54cb19fde6e0cad0c7bc81c990e
Marc Gascon <marc.gascon@wuaki.tv>
2016-07-04 10:20:29 +0200

    GET / was only accessible from localhost
    After that, you can only do the GET / from localhost.
    This is the default configuration of sinatra, so I have
    to change it putting a bind to 0.0.0.0/0 into the ruby app.

--- hello-world.rb
+++ hello-world.rb
@@ -3,6 +3,8 @@ require 'redis'
 
 redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
 
+set :bind, '0.0.0.0'
+
 get '/' do
   redis.ping
   "Hello World!"
